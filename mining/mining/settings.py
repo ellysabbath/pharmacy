f8f39339f5b0ef5dd8ed0e8ed49d18419782d95e
elly <mwananjelaelisha36@gmail.com>
2025-05-30 02:20:31 +0300

    updated email and URLs

--- mining/mining/settings.py
+++ mining/mining/settings.py
@@ -148,8 +148,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'iyumbusda@gmail.com'  # Replace with your email address
-EMAIL_HOST_PASSWORD = 'wdhdezvjzbegvdjs'  # Replace with your email password
+EMAIL_HOST_USER = 'phrmcsystem@gmail.com'  # Replace with your email address
+EMAIL_HOST_PASSWORD = 'fnnqwtxhtruwpept'  # Replace with your email password
 
 # settings.py
 
